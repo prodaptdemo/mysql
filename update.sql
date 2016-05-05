b46d9fd73abcc483201b15c84a1df918fca22647
rakesh635 <rakesh635@gmail.com>
2016-05-05 18:38:19 +0530

    Create update.sql

---
+++ update.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 05, 2016 at 10:54 AM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 5.6.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wordpress`
+--
+/*CREATE DATABASE IF NOT EXISTS `wordpress` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;*/
+USE `wordpress`;
+
+CREATE TABLE `wp_users1` (
+  `ID` bigint(20) UNSIGNED NOT NULL,
+  `user_login` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `user_pass` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `user_nicename` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `wp_users1` (`ID`, `user_login`, `user_pass`, `user_nicename`, `user_email`, `user_url`, `user_registered`, `user_activation_key`, `user_status`, `display_name`) VALUES
+(1, 'prodaptadmin', '$P$B9g1N9Yz2EGm1wC2RJHTsCx.nZp1Nz/', 'prodaptadmin', 'rakeshkumar.s@prodapt.com', '', '2016-03-15 06:00:48', '', 0, 'prodaptadmin');
+
+ALTER TABLE wp_options ADD testcol varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '';
